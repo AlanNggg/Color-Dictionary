cd2b0a85fd58926b963dfc63ce1f8f15f8c6f247
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-11-04 04:01:19 +0800

    Update dictionary.dart

--- lib/dictionary.dart
+++ lib/dictionary.dart
@@ -1,35 +1,56 @@
 import 'package:flutter/material.dart';
 
-class Dictionary extends StatelessWidget {
-  final List<String> _dictionaries = [];
+class Dictionaries extends StatefulWidget {
 
-  Dictionary(List<String> _dictionaries) {
-    this._dictionaries.addAll(_dictionaries.toList());
+  final List<Dictionary> dictionaries;
+
+  Dictionaries(
+    Key key,
+    this.dictionaries,
+  ):super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _DictionariesState();
+
+}
+
+class _DictionariesState extends State<Dictionaries> {
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new ListView(
-      children: _dictionaries
-          .map((element) => new Card(
-              elevation: 5.0,
-              margin: EdgeInsets.all(20.0),
-              child: new Padding(
-                padding: new EdgeInsets.all(40.0),
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Text(
-                      element,
-                      style: TextStyle(fontSize: 30.0),
-                      textAlign: TextAlign.start,
-                    ),
-                  ],
-                )
-              ),
+    return ListView.builder(
+      itemCount: widget.dictionaries.length,
+      itemBuilder: (context, index) {
+        return new Card(
+          elevation: 5.0,
+          margin: EdgeInsets.all(20.0),
+          child: new Padding(
+            padding: new EdgeInsets.all(40.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(
+                  widget.dictionaries[index].title,
+                  style: TextStyle(fontSize: 30.0),
+                  textAlign: TextAlign.start,
+                ),
+              ],
             )
-          )
-          .toList(),
+          ),
+        );
+      },
     );
   }
+}
+
+class Dictionary {
+  final int id;
+  final String title;
+
+  Dictionary(this.id, this.title);
 }
