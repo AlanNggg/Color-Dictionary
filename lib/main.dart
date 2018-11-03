6b80f435a97d781ab4854f6d59edd588bb6417bf
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-11-03 21:42:21 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dictionary_manager.dart';
+
 void main() => runApp(new MainPage());
 
 class MainPage extends StatelessWidget {
@@ -26,46 +28,27 @@ class Navigation extends State<NavigationPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: Text("Dictionary"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search), 
-            onPressed: () {
-              showSearch(context: context, delegate: WordSearch());
-            }
-          )
-        ],
-      ),
+      
       body: new PageView(
         children: [
-          new Container(
-              child: new Column(
+          // My Dictionary
+          new Scaffold(
+              appBar: AppBar(
+                title: Text("你的單字本"),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.search), 
+                    onPressed: () {
+                      showSearch(context: context, delegate: DictionarySearch());
+                    }
+                  )
+                ],
+              ),
+              body: new Column(
                 children: <Widget>[
+                  // Add Dictionary by using Dictionary Manager
                   new Expanded(
-                    child: new Center(
-                      child: new Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          new Container(
-                            margin: const EdgeInsets.only(right: 20.0),
-                            child: new FloatingActionButton(
-                              onPressed: () {},
-                              child: new Icon(Icons.add),
-                            )
-                          ),
-                          new Text(
-                            "Create Dictionary",
-                            style: new TextStyle(
-                              fontFamily: "Roboto",
-                              fontSize: 30.0,
-                              color: Colors.grey
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+                    child: DictionaryManager("My Vocabulary", "建立單字本"),
                   ),
                   new SizedBox(
                     height: 10.0,
@@ -77,36 +60,78 @@ class Navigation extends State<NavigationPage> {
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: new Center(
-                      child: new Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          new Container(
-                            margin: const EdgeInsets.only(right: 20.0),
-                            child: new FloatingActionButton(
-                              onPressed: () {},
-                              child: new Icon(Icons.add),
-                            )
-                          ),
-                          new Text(
-                            "Create Dictionary",
-                            style: new TextStyle(
-                              fontFamily: "Roboto",
-                              fontSize: 30.0,
-                              color: Colors.grey
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+                  new Expanded(
+                    child: DictionaryManager("Recommand Vocabulary", "取得單字本"),
                   )
                 ],
               )
           ),
-          new Container(
-              child: new Column(
+          new Scaffold(
+            appBar: AppBar(
+              title: Text("新增單字本"),
+            ),
+            body: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new Expanded(
+                  child: new Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: new Card(
+                      elevation: 7.0,
+
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      
+                      child: new Padding(
+                        padding: EdgeInsets.only(top: 20.0, bottom: 10.0, left: 20.0, right: 20.0),
+                        child: new Column(
+                          children: <Widget>[
+                            new Text("建立單字本", style: TextStyle(fontSize: 30.0),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                new Expanded(
+                  child: new Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: new Card(
+                      elevation: 7.0,
+
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      
+                      child: new Padding(
+                        padding: EdgeInsets.only(top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
+                        child: new Column(
+                          children: <Widget>[
+                            new Text("分享單字本", style: TextStyle(fontSize: 30.0),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          // Search
+          new Scaffold(
+              appBar: AppBar(
+                title: Text("字典"),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.search), 
+                    onPressed: () {
+                      showSearch(context: context, delegate: WordSearch());
+                    }
+                  )
+                ],
+              ),
+              body: new Column(
                 children: <Widget>[
                   new Expanded(
                     child: new Center(
@@ -117,6 +142,7 @@ class Navigation extends State<NavigationPage> {
                           new Container(
                             margin: const EdgeInsets.only(right: 20.0),
                             child: new FloatingActionButton(
+                              heroTag: "Word",
                               onPressed: () {},
                               child: new Icon(Icons.add),
                             )
@@ -143,7 +169,7 @@ class Navigation extends State<NavigationPage> {
                       ),
                     ),
                   ),
-                  Expanded(
+                  new Expanded(
                     child: new Center(
                       child: new Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -152,6 +178,7 @@ class Navigation extends State<NavigationPage> {
                           new Container(
                             margin: const EdgeInsets.only(right: 20.0),
                             child: new FloatingActionButton(
+                              heroTag: "Meaning",
                               onPressed: () {},
                               child: new Icon(Icons.add),
                             )
@@ -182,11 +209,15 @@ class Navigation extends State<NavigationPage> {
         items: [
           new BottomNavigationBarItem(
             icon: Icon(Icons.library_books),
-            title: Text('Dictionary'),
+            title: Text('個人字典'),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            title: Text('新增個人字典'),
           ),
           new BottomNavigationBarItem(
             icon: Icon(Icons.search),
-            title: Text('Search'),
+            title: Text('查字典'),
           ),
         ],
       ),
@@ -212,6 +243,10 @@ class Navigation extends State<NavigationPage> {
 }
 
 class WordSearch extends SearchDelegate<String> {
+  final recentCities = [
+    "new Delhi",
+    "Fairdabad"
+  ];
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -244,9 +279,62 @@ class WordSearch extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
+    return ListView.builder(
+      itemBuilder: (context,index) => ListTile(
+        leading: Icon(Icons.book),
+        title: Text(recentCities[index]),
+      ),
+      itemCount: recentCities.length,
+    );
   }
     
 }
 
+class DictionarySearch extends SearchDelegate<String> {
+  final recentCities = [
+    "new Delhi",
+    "Fairdabad"
+  ];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear), 
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context,index) => ListTile(
+        leading: Icon(Icons.book),
+        title: Text(recentCities[index]),
+      ),
+      itemCount: recentCities.length,
+    );
+  }
+    
+}
 
