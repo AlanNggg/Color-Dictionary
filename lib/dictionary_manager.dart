55968de8e627cb2ca050070731a394c4655fceb8
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-11-04 04:01:02 +0800

    Update dictionary_manager.dart

--- lib/dictionary_manager.dart
+++ lib/dictionary_manager.dart
@@ -1,70 +1,83 @@
 import 'package:flutter/material.dart';
 
-import './dictionary.dart';
+import './dictionaries.dart';
 
 class DictionaryManager extends StatefulWidget {
+  final List<Dictionary> dictionaries;
 
   final String type;
   final String title;
 
-  DictionaryManager(this.type, this.title);
+  DictionaryManager(
+    Key key,
+    this.dictionaries,
+    this.type, 
+    this.title
+  ):super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _DictionaryManagerState();
-  }
+  State<StatefulWidget> createState() => _DictionaryManagerState();
 }
 
 class _DictionaryManagerState extends State<DictionaryManager> {
-  List<String> _dictionaries = [];
 
   String type;
-
   String title;
+  int id;
 
   @override
   void initState() {
     type = widget.type;
     title = widget.title;
+
+    if (widget.dictionaries.length > 0) {
+      id = widget.dictionaries[widget.dictionaries.length - 1].id;
+    } else {
+      id = 0;
+    }
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_dictionaries.length <= 0) {
+    if (widget.dictionaries.length <= 0) {
       return new Center(
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  new Container(
-                    margin: const EdgeInsets.only(right: 20.0),
-                    child: new FloatingActionButton(
-                      heroTag: type,
-                      onPressed: () {
-                        setState(() {
-                          _dictionaries.add("New Dictionary");
-                        });
-                      },
-                      child: new Icon(Icons.add),
-                    )
-                  ),
-                  new Text(
-                    title,
-                    style: new TextStyle(
-                      fontFamily: "Roboto",
-                      fontSize: 30.0,
-                      color: Colors.grey
-                    ),
-                  )
-                ],
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 20.0),
+              child: new FloatingActionButton(
+                heroTag: type,
+                onPressed: () {
+                  setState(() {
+                    widget.dictionaries.add(Dictionary(id, "New Dictionary"));
+                  });
+                },
+                child: new Icon(Icons.add),
               )
+            ),
+            new Text(
+              title,
+              style: new TextStyle(
+                fontFamily: "Roboto",
+                fontSize: 30.0,
+                color: Colors.grey
+              ),
+            )
+          ],
+        )
       );
     } else {
       return new Column(
         children: <Widget>[
           new Expanded(
-            child: Dictionary(_dictionaries)
+            child: new Dictionaries(
+              this.widget.key,
+              this.widget.dictionaries,
+            )
           )
         ],
       );
