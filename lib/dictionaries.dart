997091fa57f6175139fc5420bceb5bb4022e5ed1
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-11-04 22:10:27 +0800

    Update dictionaries.dart

--- lib/dictionaries.dart
+++ lib/dictionaries.dart
@@ -26,31 +26,49 @@ class _DictionariesState extends State<Dictionaries> {
     return ListView.builder(
       itemCount: widget.dictionaries.length,
       itemBuilder: (context, index) {
-        return new Card(
-          elevation: 5.0,
-          margin: EdgeInsets.all(20.0),
-          child: new InkWell( 
-            onTap:(){
-              Scaffold.of(context).showSnackBar(SnackBar(
-                content: Text('Tap'),
-              ));
-            },
-            child: new Padding(
-              padding: new EdgeInsets.all(40.0),
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(
-                    widget.dictionaries[index].title,
-                    style: TextStyle(fontSize: 30.0),
-                    textAlign: TextAlign.start,
-                  ),
-                ],
+        return new Stack( 
+          fit: StackFit.passthrough,
+          children: <Widget>[     
+            new Card(
+              margin: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+              elevation: 5.0,
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: new InkWell( 
+                onTap:(){
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text('Tap'),
+                  ));
+                },
+                child: new Padding(
+                  padding: new EdgeInsets.all(40.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text(
+                        widget.dictionaries[index].title,
+                        style: TextStyle(fontSize: 30.0),
+                        textAlign: TextAlign.start,
+                      ),
+                    ],
+                  )
+                ),
               )
             ),
-          )
+            new Container(
+              alignment: new FractionalOffset(1.0, 0.0),
+              margin: const EdgeInsets.only(left: 24.0),
+              child: new Image(
+                image: new AssetImage(widget.dictionaries[index].image),
+                height: 70.0,
+                width: 70.0,
+              ),
+            ),
+          ] 
         );
-      },
+      } //itemBuilder
     );
   }
 }
@@ -58,6 +76,7 @@ class _DictionariesState extends State<Dictionaries> {
 class Dictionary {
   final int id;
   final String title;
+  final String image;
 
-  Dictionary(this.id, this.title);
+  Dictionary(this.id, this.title, this.image);
 }
